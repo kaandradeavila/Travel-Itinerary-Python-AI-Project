5702a6e978b513a912f5baf1a2b934da555df0bf
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2025-04-18 01:32:58 -0400

    committing final touches

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ def display_current_weather(location):
   temperature = round(response_data['temperature']['current'])
   condition = response_data['condition']['description']
 
-  print(f"\nThe current temperature in [bold]{location}[/bold] is: [bold]{temperature}°F[/bold], {condition}/n")
+  print(f"\nThe current temperature in [bold blue]{location}[/bold blue] is: [bold]{temperature}°F[/bold], [green]{condition}[/green]/n")
 
 
-def generate_travel_itinerary(origin, destination, duration):
+def generate_travel_itinerary(origin, destination, duration, currency):
   """ Generates a travel itinerary between 2 different places using AI """
   print(f"\n\nGenerating itinerary from {origin} to {destination}...\n")
   ai_api_key = "424369doa037d0347bft3cfcc8cef956"
   ai_context = "You are a renowned travel specialist who knows the best tourist spots around the world!"
-  ai_prompt = f"Generate a travel itinerary from {origin} to {destination} in {duration} days. This is a road, keep it short, less than 15 lines. Add some emojis to make it more readable, but no more than 5 emojis. Add an estimated price for each day in American dollars. Thanks!"
+  ai_prompt = f"Generate a travel itinerary from {origin} to {destination} in {duration} days. This is a road, keep it short, less than 15 lines. Add some emojis to make it more readable. Add a main title to the itinerary and divide each day with a line. Add an estimated price for each day in {currency}. Thanks!"
   ai_api_url = f"https://api.shecodes.io/ai/v1/generate?prompt={ai_prompt}&context={ai_context}&key={ai_api_key}"
 
   response = requests.get(ai_api_url)
@@ -45,14 +45,13 @@ welcome()
 origin = input("What city does your trip start from? ").strip()
 destination = input("What city are you going to? ").strip()
 duration = input("How many days will your trip last? (Enter a number only, i.e 5) ").strip()
+currency = input("What currency do you use (i.e. euro, dollar...)? ")
 
 # Validating user input
-if origin and destination and duration.isdigit():
+if origin and destination and duration.isdigit() and currency:
   display_current_weather(origin)
   display_current_weather(destination)
-  generate_travel_itinerary(origin, destination, duration)
+  generate_travel_itinerary(origin, destination, duration, currency)
   credit()
 else:
   print("Please try again. Make sure to enter valid information.")
-
-# Final touches (add credit, few improvements, potential)
