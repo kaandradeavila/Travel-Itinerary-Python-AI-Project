d8160134ef4ad82bb71558442dd0fd494e43e046
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2025-04-17 23:53:14 -0400

    Committing user input validations

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ import requests
 from rich import print
 from rich.markdown import Markdown
 
-# Get user input (origin, destination, duration)
 
+def generate_travel_itinerary(origin, destination, duration):
+  print("In Progress...")
+
+# Get user input (origin, destination, duration)
+origin = input("What city does your trip start from? ").strip()
+destination = input("What city are you going to? ").strip()
+duration = input("How many days will your trip last? (Enter a number only, i.e 5) ").strip()
+
+# Validating user input
+if origin and destination and duration.isdigit():
+  print("Calculating itinirary...")
+else:
+  print("Please try again. Make sure to enter valid information.")
 
 # Call the SheCodes AI API (to get itinerary)
 
