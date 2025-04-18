92234f3a2420d784d1fb07ef5573a48cb0785071
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2025-04-17 23:33:26 -0400

    initial commit
    Setting up the project

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+from rich import print
+from rich.markdown import Markdown
+
+# Get user input (origin, destination, duration)
+
+
+# Call the SheCodes AI API (to get itinerary)
+
+
+# Display the itinerary
+
+
+# Call the Weather API
+
+
+# Call the SheCodes Weather API
+
+
+# Display the weather in the origin and destination
+
+
+# Final touches (add credit, few improvements, potential)
+
+
+print("Hello World")
